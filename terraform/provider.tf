253c660a983179583ba910b402f5c8672a21e3a7
Kenny-AngJY <fire_light2010@hotmail.com>
2025-12-29 21:59:59 +0800

    Updated aws provider and terraform required version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.20.0"
+      version = "~> 6.25.0"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -10,7 +10,7 @@ terraform {
     }
   }
 
-  required_version = "~> 1.13"
+  required_version = "~> 1.14"
 
   # backend "s3" {
   #   bucket  = "xxx"
